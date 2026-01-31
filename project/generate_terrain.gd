60ae65fbde3d743fe35effd24d9127aa6508f742
aikm94 <aikm94@ymail.ne.jp>
2026-01-31 16:51:03 +0900

    Bugfix: heightmap being set to ints. Optimization: ignore 0 pixels.
    Was casting heights to ints, losing a ton of info.
    Skipping pixels with a value of 0, assuming
    they're the ocean right now. Will see how that goes.

--- project/generate_terrain.gd
+++ project/generate_terrain.gd
@@ -1,43 +1,32 @@
+## heightmaps come from https://manticorp.github.io/unrealheightmap.
+## pixel values range from 0 to 1. when creating a heightmap, it will say the min/max values.
+## make sure to record this value in a text file or something because its different per height map.
 extends Node
 
-@export var heightmap_path: String = "res://assets/heightmaps/falklands/-51_775_-59_436_12_13000_9000.png"
-@export var output_folder: String = "res://assets/terrain/falklands/"
-@export var height_scale: float = 10.0
-@export var cell_size: float = 1.0
+const HEIGHTMAP_PATH: String = "res://assets/heightmaps/vancouver_downtown/49_257_-123_183_15_4094_4094_16bit.png"
+const OUTPUT_FOLDER: String = "res://assets/terrain/vancouver_downtown/"
+const HEIGHT_SCALE: float = 130.0
+const CELL_SIZE: float = 1.0
 
+## the size of a single chunk in meters.
+## each meter of the heighmap is 1 pixel
 const SMALLEST_CHUNK_SIZE: int = 256
+## the number of pixels to skip when generating a chunk.
+## higher number means lower detail.
 #const LOD_DIVIDERS: Array[int] = [1,2,4,8,16]
-const LOD_DIVIDERS: Array[int] = [8]
-
+const LOD_DIVIDERS: Array[int] = [1,2]
 
 
 func _ready():
 	generate_heightmap()
 
-func load_terrain_from_disk() -> void:
-	var lod_divider: int = 8
-	for x in range(6):
-		for y in range(4):
-			var terrain_path: String = "res://assets/terrain/falklands"
-			var mesh_id: String = "%d_%d_lod%d" % [x,y,lod_divider]
-			var mesh_path: String = terrain_path.path_join(mesh_id) + ".res"
-			if ResourceLoader.exists(mesh_path):
-				var mesh_resource: ArrayMesh = load(mesh_path)
-				var mesh_instance: MeshInstance3D = MeshInstance3D.new()
-				mesh_instance.name = mesh_id
-				mesh_instance.mesh = mesh_resource
-				mesh_instance.position = Vector3(
-					x*SMALLEST_CHUNK_SIZE*lod_divider,
-					0,
-					y*SMALLEST_CHUNK_SIZE*lod_divider)
-
 ## TODO: optimize away flat areas.
 ## TODO: remove ocean?
 func generate_heightmap() -> void:
 	var img: Image = Image.new()
-	var err = img.load(heightmap_path)
+	var err = img.load(HEIGHTMAP_PATH)
 	if err != OK:
-		push_error("Failed to load heightmap: %s" % heightmap_path)
+		push_error("Failed to load heightmap: %s" % HEIGHTMAP_PATH)
 		return
 	
 	var width: int = img.get_width()
@@ -49,41 +38,51 @@ func generate_heightmap() -> void:
 			for chunk_y in range(0, height, lod_size):
 				if (chunk_x+lod_size >= width ) || (chunk_y+lod_size >= height):
 					continue
-				
-				var chunk_index_denominator = lod_divider*SMALLEST_CHUNK_SIZE
-				var chunk_id: String = "%d_%d_lod%d" %[chunk_x/chunk_index_denominator, chunk_y/chunk_index_denominator, lod_divider]
-				print("preparing chunk: ", chunk_id)
-				var st = SurfaceTool.new()
-				st.begin(Mesh.PRIMITIVE_TRIANGLES)
-				
-				# for each chunk	
-				# create a mesh using the image,
-				# and skip over lod_divider vertices
-				# make sure the mesh has its top left corner at 0,0
-				for pixel_x in range(chunk_x, chunk_x+lod_size, lod_divider):
-					for pixel_y in range(chunk_y, chunk_y+lod_size, lod_divider):
-					
-						var h00: int = img.get_pixel(pixel_x, pixel_y).r * height_scale
-						var h10: int = img.get_pixel(pixel_x + lod_divider, pixel_y).r * height_scale
-						var h01: int = img.get_pixel(pixel_x, pixel_y + lod_divider).r * height_scale
-						var h11: int = img.get_pixel(pixel_x + lod_divider, pixel_y + lod_divider).r * height_scale
-						
-						# reset chunk position back to (0,0).
-						var triangle_width: float = cell_size * lod_divider
-						var x0: int = (pixel_x * triangle_width) - chunk_x
-						var y0: int = (pixel_y * triangle_width) - chunk_y
-						
-						## Two triangles per cell
-						st.add_vertex(Vector3(x0, h00, y0))
-						st.add_vertex(Vector3(x0 + triangle_width, h10, y0))
-						st.add_vertex(Vector3(x0, h01, y0 + triangle_width))
+				generate_chunk(img, chunk_x, chunk_y, lod_divider, lod_size);
 
-						st.add_vertex(Vector3(x0 + triangle_width, h10, y0))
-						st.add_vertex(Vector3(x0 + triangle_width, h11, y0 + triangle_width))
-						st.add_vertex(Vector3(x0, h01, y0 + triangle_width))
-						
-				st.generate_normals()
-				var mesh: ArrayMesh = st.commit()
-				var filename_resource: String = "%s.res" % chunk_id
-				var output_path: String = output_folder.path_join(filename_resource)
-				ResourceSaver.save(mesh, output_path)
+
+func generate_chunk(img: Image, x: int, y: int, lod_divider: int, lod_size: int) -> void:
+	var chunk_index_denominator = lod_divider*SMALLEST_CHUNK_SIZE
+	var chunk_id: String = "%d_%d_lod%d" % [x/chunk_index_denominator, y/chunk_index_denominator, lod_divider]
+	print("preparing chunk: ", chunk_id)
+	var st = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	# for each chunk
+	# create a mesh using the image,
+	# and skip over lod_divider vertices
+	# make sure the mesh has its top left corner at 0,0
+	for pixel_x in range(x, x+lod_size, lod_divider):
+		for pixel_y in range(y, y+lod_size, lod_divider):
+			var h00 = img.get_pixel(pixel_x, pixel_y).r * HEIGHT_SCALE
+			var h10 = img.get_pixel(pixel_x + lod_divider, pixel_y).r * HEIGHT_SCALE
+			var h01 = img.get_pixel(pixel_x, pixel_y + lod_divider).r * HEIGHT_SCALE
+			var h11 = img.get_pixel(pixel_x + lod_divider, pixel_y + lod_divider).r * HEIGHT_SCALE
+			
+			# lets skip oceans
+			if (h00 == 0
+				and h10 == 0 
+				and h01 == 0 
+				and h11 == 0):
+				continue;
+			
+			# reset chunk position back to (0,0).
+			var triangle_width: float = CELL_SIZE * lod_divider
+			var x0: int = (pixel_x * triangle_width) - x
+			var y0: int = (pixel_y * triangle_width) - y
+			
+			## Two triangles per cell
+			st.add_vertex(Vector3(x0, h00, y0))
+			st.add_vertex(Vector3(x0 + triangle_width, h10, y0))
+			st.add_vertex(Vector3(x0, h01, y0 + triangle_width))
+
+			st.add_vertex(Vector3(x0 + triangle_width, h10, y0))
+			st.add_vertex(Vector3(x0 + triangle_width, h11, y0 + triangle_width))
+			st.add_vertex(Vector3(x0, h01, y0 + triangle_width))
+			
+	st.generate_normals()
+	var mesh: ArrayMesh = st.commit()
+	var filename_resource: String = "%s.res" % chunk_id
+	var output_path: String = OUTPUT_FOLDER.path_join(filename_resource)
+	ResourceSaver.save(mesh, output_path)
+	return
