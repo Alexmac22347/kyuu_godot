db7b2d6736b2d3612e2c797a981894f92bde1880
aikm94 <aikm94@ymail.ne.jp>
2026-02-07 11:02:46 +0900

    whoops, wrote my heightmap imported code assuming that the size of each image was the same. this was wrong and resulted in nulls in the array

--- project/generate_terrain.gd
+++ project/generate_terrain.gd
@@ -1,6 +1,12 @@
 ## heightmaps come from https://manticorp.github.io/unrealheightmap.
+## DO NOT ASSUME ALL IMAGE SAME DIMENSIONS
 ## absolutely no texturing is done here in order to preserve vram.
 ## thats because the same texture can be used for multiple chunks.
+## EXR format:
+## Pixel space is a 2D coordinate system with x increasing from left to right
+## and y increasing from top to bottom.
+## pixels are data samples, taken at integer coordinate locations in pixel space.
+## (https://download.nvidia.com/developer/GPU_Gems/CD_Image/Image_Processing/OpenEXR/OpenEXR-1.0.6/doc/)
 extends Node
 
 const HEIGHTMAP_PATH: String = "res://assets/heightmaps/vancouver/"
@@ -14,7 +20,8 @@ const WATER_CUTOFF: float = -10
 const SMALLEST_CHUNK_SIZE: int = 256
 ## the number of pixels to skip when generating a chunk.
 ## higher number means lower detail.
-const LOD_DIVIDERS: Array[int] = [1,2,4,8,16]
+#const LOD_DIVIDERS: Array[int] = [1,2,4,8,16]
+const LOD_DIVIDERS: Array[int] = [1,4,16]
 
 
 func _ready():
@@ -28,8 +35,8 @@ func generate_heightmap() -> void:
 	## from there we can generate the meshes.
 	var raw_pixel_values: Array = generate_pixel_values_from_heightmap_folder(HEIGHTMAP_PATH)
 
-	var height: int = raw_pixel_values.size()
-	var width: int = raw_pixel_values[0].size()
+	var width: int = raw_pixel_values.size()
+	var height: int = raw_pixel_values[0].size()
 
 	for lod_divider in LOD_DIVIDERS:
 		var lod_size: int = lod_divider * SMALLEST_CHUNK_SIZE
@@ -39,6 +46,8 @@ func generate_heightmap() -> void:
 					continue
 				generate_chunk(raw_pixel_values, chunk_x, chunk_y, lod_divider, lod_size)
 
+## returns 2D array of pixel values
+## retval[row][column]. 0,0 is the upper left corner.
 func generate_pixel_values_from_heightmap_folder(heightmap_folder: String) -> Array:
 	## turn all those exr files into one big 2D array of heights
 	var dir: DirAccess = DirAccess.open(heightmap_folder)
@@ -51,38 +60,41 @@ func generate_pixel_values_from_heightmap_folder(heightmap_folder: String) -> Ar
 	## first just get the size of a single .exr image and how many images
 	## there are in the x and y direction.
 	dir.list_dir_begin()
-	var tile_width_pixels: int = 0
-	var tile_height_pixels: int = 0
-	var max_tile_y_offset: int = 0
-	var max_tile_x_offset: int = 0
+	var file_width_pixels: int = 0
+	var file_height_pixels: int = 0
+	var num_files_x: int = 0
+	var num_files_y: int = 0
 	var file_name = dir.get_next()
 	while file_name != "":
 		if !file_name.ends_with(".exr"):
 			file_name = dir.get_next()
 			continue
-		if tile_width_pixels == 0 && tile_height_pixels == 0:
+		if file_width_pixels == 0 && file_height_pixels == 0:
 			var img: Image = Image.new()
 			img.load(heightmap_folder+file_name)
-			tile_width_pixels = img.get_width()
-			tile_height_pixels = img.get_height()
+			file_width_pixels = img.get_width()
+			file_height_pixels = img.get_height()
 		var regex_result = regex.search(file_name)
-		var tile_y_offset: int = regex_result.get_string(1).to_int()
-		var tile_x_offset: int = regex_result.get_string(2).to_int()
-		if tile_y_offset > max_tile_y_offset:
-			max_tile_y_offset = tile_y_offset
-		if tile_x_offset > max_tile_x_offset:
-			max_tile_x_offset = tile_x_offset
+		## the second coord is left/right/width on the file names
+		## i know this because the falklands heightmap is a lot wider than it is tall,
+		## and the second coord goes up to 20 (as opposed to 10)
+		var file_x_offset_1based: int = regex_result.get_string(2).to_int()
+		var file_y_offset_1based: int = regex_result.get_string(1).to_int()
+		if file_x_offset_1based > num_files_x:
+			num_files_x = file_x_offset_1based
+		if file_y_offset_1based > num_files_y:
+			num_files_y = file_y_offset_1based
 		file_name = dir.get_next()
 	dir.list_dir_end()
 
 	## initialize the array
 	var pixel_array: Array[Array]
-	pixel_array.resize(tile_height_pixels*max_tile_y_offset)
+	pixel_array.resize(file_width_pixels*num_files_x)
 	for i in range(pixel_array.size()):
 		pixel_array[i] = []
-		pixel_array[i].resize(tile_width_pixels*max_tile_x_offset)
+		pixel_array[i].resize(file_height_pixels*num_files_y)
 
-	print("initialized pixel array with size ", pixel_array.size(), " x ", pixel_array[1111].size())
+	print("pixel array width: ", pixel_array.size(), " height: ", pixel_array[0].size())
 
 	dir.list_dir_begin()
 	file_name = dir.get_next()
@@ -93,19 +105,25 @@ func generate_pixel_values_from_heightmap_folder(heightmap_folder: String) -> Ar
 
 		var regex_result = regex.search(file_name)
 		## the files use 1 based indexing, need to subtract 1 to convert to 0 based.
-		var tile_y_offset: int = regex_result.get_string(1).to_int() - 1
-		var tile_x_offset: int = regex_result.get_string(2).to_int() - 1
+		var file_y_offset: int = regex_result.get_string(1).to_int() - 1
+		var file_x_offset: int = regex_result.get_string(2).to_int() - 1
 
 		var img: Image = Image.new()
 		img.load(heightmap_folder+file_name)
-		print("loaded image: ", file_name)
+
+		if file_x_offset == 9 and file_y_offset == 0:
+			var alex = true
 
 		for y in range(img.get_height()):
 			for x in range(img.get_width()):
-				var true_y: int = (tile_y_offset*tile_height_pixels) + y
-				var true_x: int = (tile_x_offset*tile_width_pixels) + x
+				var true_x: int = (file_x_offset*file_width_pixels) + x
+				var true_y: int = (file_y_offset*file_height_pixels) + y
+				if true_x == 2048:
+					var bob = true
+
 				var pixel: Color = img.get_pixel(x,y)
-				pixel_array[true_y][true_x] = pixel.r
+
+				pixel_array[true_x][true_y] = pixel.r
 		file_name = dir.get_next()
 
 	dir.list_dir_end()
@@ -125,10 +143,10 @@ func generate_chunk(pixel_values: Array[Array], x: int, y: int, lod_divider: int
 	# make sure the mesh has its top left corner at 0,0
 	for pixel_x in range(x, x+lod_size, lod_divider):
 		for pixel_y in range(y, y+lod_size, lod_divider):
-			var h00: float = pixel_values[pixel_y][pixel_x] * HEIGHT_SCALE
-			var h10: float = pixel_values[pixel_y + lod_divider][pixel_x] * HEIGHT_SCALE
-			var h01: float = pixel_values[pixel_y][pixel_x + lod_divider] * HEIGHT_SCALE
-			var h11: float = pixel_values[pixel_y + lod_divider][pixel_x + lod_divider] * HEIGHT_SCALE
+			var h00: float = pixel_values[pixel_x][pixel_y] * HEIGHT_SCALE
+			var h10: float = pixel_values[pixel_x + lod_divider][pixel_y] * HEIGHT_SCALE
+			var h01: float = pixel_values[pixel_x][pixel_y + lod_divider] * HEIGHT_SCALE
+			var h11: float = pixel_values[pixel_x + lod_divider][pixel_y + lod_divider] * HEIGHT_SCALE
 
 			if (h00 <= WATER_CUTOFF
 				and h10 <= WATER_CUTOFF
@@ -166,6 +184,14 @@ func generate_chunk(pixel_values: Array[Array], x: int, y: int, lod_divider: int
 
 	st.generate_normals()
 	var mesh: ArrayMesh = st.commit()
+
+	## this is for debugging, i should not apply materials to the mesh.
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = preload("res://assets/textures/grass_81/grass_81_128.png")
+	material.roughness = 0.8
+	material.metallic = 0.0
+
+	mesh.surface_set_material(0, material)
 
 	var filename_resource: String = "%s.res" % chunk_id
 	var output_path: String = OUTPUT_FOLDER.path_join(filename_resource)
